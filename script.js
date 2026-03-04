682d8a3b830553d2da27ae1cf8cb1c61c34e588b
trujillozhiannmatthewschleiden-sudo <trujillo.zhiannmatthew.schleiden@gmail.com>
2026-03-04 22:27:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function generatePassword() {
+
+    var length = document.getElementById("length").value;
+
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    var password = "";
+
+    for (var i = 0; i < length; i++) {
+
+        var randomNumber = Math.floor(Math.random() * characters.length);
+
+        password += characters[randomNumber];
+    }
+
+    document.getElementById("result").textContent = password;
+}
